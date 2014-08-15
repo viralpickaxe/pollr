ca8fff975d2d6d492f41ee8246587e379309e79b
Jamie Davies <viralpickaxe@gmail.com>
2014-08-16 00:27:40 +0100

    Simple creating

--- public/assets/js/newquestion.script.js
+++ public/assets/js/newquestion.script.js
@@ -1,7 +1,7 @@
 var socket = io();
 
 $(document).ready(function(){
-  $('#newquestionform .question').focus();
+  $('#newquestionform .question').focus().keyup();
 });
 
 $('#newquestionform').on('submit',function(){
@@ -10,9 +10,19 @@ $('#newquestionform').on('submit',function(){
   
   if(!$askbtn.attr('disabled')){
     console.log('submitted');
-    $askbtn.addClass('active');
+    $askbtn.attr('disabled',true).addClass('active');
+    $question.attr('disabled',true);
     setTimeout(function(){
-      $askbtn.removeClass('active');
+      socket.emit('question:create:req',{name:$question.val()});
+      socket.on('question:create:res',function(data){
+        if(data.created){
+          window.location.href = '/' + data.questiondata.url;
+        } else {
+          $askbtn.attr('disabled',false).removeClass('active');
+          $question.attr('disabled',false);
+          console.log('error');
+        }
+      });
     },100);
   }
 });
--- server.js
+++ server.js
@@ -9,12 +9,29 @@ var app = express();
 var server = http.Server(app);
 var io = socket(server);
 
+var test = 'hello';
+
 server.listen(port, function(){
   console.log('listening on *:' + port);
 });
 
 app.get('/', function(req, res){
   res.sendfile('./views/newquestion.html');
+  io.on('connection', function(socket){
+    socket.on('question:create:req', function(data){
+      var question = {
+        name: data.name,
+        url: encodeURIComponent(data.name.replace(/ /g,'-'))
+      }
+      socket.emit('question:create:res',{created:true,questiondata:question});
+    });
+  });
+});
+
+app.get('/:pollurl', function(req, res){
+  res.sendfile('./views/poll.html');
+  var pollurl = req.params.pollurl;
+  console.log(pollurl);
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
